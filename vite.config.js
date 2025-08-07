d907dae68a7307aec01325bf175beae0528c5e26
Dave Seidman <daveseidman@Daves-MacBook-Pro.local>
2025-08-07 12:48:10 -0400

    fix route

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/brooklyn-poker-club/',
+  base: '/brooklyn-poker-club-frontend/',
   plugins: [react()],
   server: {
     port: 8080,

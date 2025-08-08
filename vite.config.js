1febdf59f313816c5c7ba62ba0e6aef55d1e3b4c
Dave Seidman <daveseidman@Daves-MacBook-Pro.local>
2025-08-08 08:01:55 -0400

    remove base, switching to custom domain

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/brooklyn-poker-club-frontend/',
   plugins: [react()],
   server: {
     port: 8080,
